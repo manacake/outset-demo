bc80ecaa69cb90b6b6c46c1c70dd1bcb4a5f0b8f
Tina Zhao <tinayzhao@gmail.com>
2018-08-30 16:57:13 -0700

    replace gfx lib

---
+++ src/pins.h
@@ -0,0 +1,49 @@
+// pins.h
+#ifndef Pins_h
+#define Pins_h
+
+// General
+#define LVL_SHIFT_EN 2
+
+// Radio
+#define RFM_CS     10
+#define RFM_INT    3
+#define RFM_RESET  49
+#define RFM_PWR_EN 5
+
+// Screen
+#define TFT_CS     27
+#define TFT_RESET  29
+#define TFT_DC     28
+#define TFT_PWR_EN 4
+
+// Trackpad
+#define TP_CS       47
+#define TP_BUTTON   23
+#define TP_RESET    25
+#define TP_SHUTDOWN 24
+
+// SD Card
+#define SD_CS    22
+#define DET_CARD 39
+
+// Keypad
+#define ROW_1 30
+#define ROW_2 31
+#define ROW_3 32
+#define ROW_4 33
+#define ROW_5 34
+#define COL_1 35
+#define COL_2 36
+#define COL_3 37
+#define COL_4 40
+#define COL_5 41
+#define COL_6 38
+#define COL_7 42
+#define COL_8 44
+#define COL_9 45
+#define COL_10 46
+#define KEYPAD_ROWS 5
+#define KEYPAD_COLS 10
+
+#endif
